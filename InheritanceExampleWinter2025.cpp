3946af949002c9628b4e4564b1f5acd3ffb05375
Kamel R <kamel.r@wayne.edu>
2025-02-12 19:04:39 -0500

    added more objects

--- InheritanceExampleWinter2025.cpp
+++ InheritanceExampleWinter2025.cpp
@@ -22,11 +22,16 @@ int main()
 	emp1->setSalary(2000);
 
 	Employee* emp2 = new Employee("1234", "james@yahoo.com", "James", 3000);
+	/*delete emp2;
+	emp2 = NULL; */
 
 	//delete emp2; 
 	//delete emp1;
 	// 
 	
+
+	Employee* emp3 = emp2; // shallow copy 
+
 	int employeeCount = 3; 
 	Employee* employeeList = new Employee[employeeCount];
 	string temp;
@@ -47,6 +52,22 @@ int main()
 		employeeList[i].printEmployeeInfo();
 		
 	}
+
+	//delete[] employeeList;
+
+	//Employee* empListCopy = employeeList;// shallow copy 
+
+	// Deep copy 
+	Employee* empListCopy = new Employee[employeeCount];
+	for (int i = 0; i < employeeCount; i++)
+	{
+		empListCopy[i].setEmail(employeeList[i].getEmail());
+		empListCopy[i].setName(employeeList[i].getName());
+		empListCopy[i].setID(employeeList[i].getID());
+		empListCopy[i].setSalary(employeeList[i].getSalary());
+
+	}
+
 
 }
 
